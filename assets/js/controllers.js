be8227b2101974eee9896613279511b59660480d
Kaitlin Saunders <kaitlinhsaunders@gmail.com>
2016-07-05 15:58:07 -0400

    Add delete functionality

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -1,8 +1,10 @@
+// credit: controller code based on a codepen by Francesca, https://codepen.io/gfrancesca/pen/gbpoxQ
+
 mainApp.controller('thingsCtrl', function($scope){
 
   $scope.things = [] //empty array
 
-  var thingText = $('input[class="toDo-thing-input"]').val();
+  // var thingText = $('input[class="toDo-thing-input"]').val(); unnecessary because of templating
 
   $scope.addThing = function (){
     $scope.things.push({'title': $scope.newThing, 'done': false})
@@ -10,13 +12,27 @@ mainApp.controller('thingsCtrl', function($scope){
   }
 
   $scope.deleteThing = function(index){
-    $scope.skills.splice(index, 1);
+
+    $scope.things.splice(index, 1); //remove item from index
   }
 
+
+
 });
+
+/*--------------failed delete attempt-----------------------------------*/
+
+// mainApp.controller('deleteCtrl', function($scope){
+//
+//   $scope.things = []
+//
+//   $scope.deleteThing = function(){
+//     $scope.skills.splice(index, 1);
+//    }
+//
+// console.log('delete ready')
 
 //nghover on li show x to delete item
 
 //ng double click
   //dropdown input, add details
-  
