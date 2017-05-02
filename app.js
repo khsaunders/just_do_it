4fba7148d5e54e3fad1c395825d2bc4ef50940ab
Kaitlin Saunders <kaitlinhsaunders@gmail.com>
2017-05-02 18:30:00 -0400

    Hack job-remove app.js from assets folder to hopefully resolve literally dumbest routing  issue ever of all time

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var mainApp = angular.module("mainApp", ['ngRoute']);
   mainApp.config(function($routeProvider) {
     $routeProvider
     .when('/home', {
-      templateUrl: '../../list.html'
+      templateUrl: 'list.html'
     })
 
     .otherwise({
