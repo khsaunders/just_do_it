ff0407690defe3cfeb6aa362c4b6a9b2faf98ef1
Kaitlin Saunders <kaitlinhsaunders@gmail.com>
2017-05-02 18:37:37 -0400

    Sooooo routing issue is due to lack of server? Not incorrect routing. Will need to set up a node.js back-end?

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+console.log("JS connected");
+
+//routing
+var mainApp = angular.module("mainApp", ['ngRoute']);
+  mainApp.config(function($routeProvider) {
+    $routeProvider
+    .when('/home', {
+      templateUrl: '../../list.html'
+    })
+
+    .otherwise({
+    redirectTo: '/home'
+    });
+  }); //end of app routing
